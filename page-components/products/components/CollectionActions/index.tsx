d7a370eabf1d54535d20cc3bc7cff872e21a3302
VietNguyen27 <nquocviet2307@gmail.com>
2023-04-21 21:58:52 +0700

    Create add new collection modal

--- page-components/products/components/CollectionActions/index.tsx
+++ page-components/products/components/CollectionActions/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Button, Flex } from '@mantine/core'
+import { useToggle } from '@mantine/hooks'
 import { Plus } from '@phosphor-icons/react'
 
+import AddNewCollectionModal from '../AddNewCollectionModal'
+
 const CollectionActions = () => {
+	const [newCollectionOpened, setNewCollectionOpened] = useToggle()
+
 	return (
 		<>
 			<Flex className="ml-auto -mt-2" gap={12}>
@@ -11,10 +16,15 @@ const CollectionActions = () => {
 					color="gray"
 					size="xs"
 					leftIcon={<Plus size={16} />}
+					onClick={() => setNewCollectionOpened(true)}
 				>
 					New Collection
 				</Button>
 			</Flex>
+			<AddNewCollectionModal
+				opened={newCollectionOpened}
+				onClose={setNewCollectionOpened}
+			/>
 		</>
 	)
 }
